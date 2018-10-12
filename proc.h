5bbcbe883b510ae01122d65468581837d0346d8a
Marvin Dela Cruz <mdela022@ucr.edu>
2018-10-12 16:19:28 +0000

    Changed exit functions according to part a

--- proc.h
+++ proc.h
@@ -49,10 +49,11 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int status;                   
 };
 
 // Process memory is laid out contiguously, low addresses first:
-//   text
+//   text;
 //   original data and bss
 //   fixed-size stack
 //   expandable heap
